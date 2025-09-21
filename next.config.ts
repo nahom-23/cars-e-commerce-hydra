ed1a5a7eb3b62019b75d688eccaf5b9b3c823276
nahom-23 <noahbirhanu23@gmail.com>
2025-09-21 16:05:50 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // 👈 enables static export
+  // other options...
 };
 
 export default nextConfig;
